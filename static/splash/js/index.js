c792bc1875bfd502b68f2484fc1f067dc38f486e
younganakin <joenjuguna53@gmail.com>
2018-09-14 13:05:32 +0300

    Added static folder.

---
+++ static/splash/js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    //var params = getSearchParameters();
+
+    //var base_grant_url = decodeURIComponent(params.base_grant_url);
+    console.log("Page ready");
+});
