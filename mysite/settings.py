f223eed3187b188bc0b88079d3f3e17dca77e87d
younganakin <joenjuguna53@gmail.com>
2018-09-18 13:14:39 +0300

    Removed logging from settings.py.

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,25 +129,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/home/joe/radius-tutorial/debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
 # CELERY STUFF
 
 
